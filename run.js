bd72cc684a29cac1f6ac9fb55ab8f90ce80b08e3
ErnestChan <ekc1@rice.edu>
2015-10-10 17:11:03 -0500

    added input string params to run

--- run.js
+++ run.js
@@ -2,14 +2,50 @@ var arDrone = require('../node/node_modules/ar-drone');
 var client = arDrone.createClient();
 var alphabet = require('./alphabet.js');
 
-//Main commands
+inputString = process.argv[2];
+if(inputString == '')
+{
+	inputString = 'MLH'
+}
+
 client.takeoff();
-alphabet.init(client);
-alphabet.drawM(client);
-alphabet.nextLetter(client);
-alphabet.drawL(client);
-alphabet.nextLetter(client);
-alphabet.drawH(client);
+len = inputString.length;
+for(var i = 0; i < len ; i++){
+	if(inputString[i] == 'M' || inputString[i] == 'm')
+	{
+		alphabet.drawM(client);
+		alphabet.nextLetter(client);
+	}
+	else if(inputString[i] == 'L' || inputString[i] == 'l')
+	{
+		alphabet.drawL(client);
+		alphabet.nextLetter(client);
+	}
+	else if(inputString[i] == 'H' || inputString[i] == 'h')
+	{
+		alphabet.drawH(client);
+		alphabet.nextLetter(client);	
+	}
+	else if(inputString[i] == 'U' || inputString[i] == 'u')
+	{
+		alphabet.drawU(client);
+		alphabet.nextLetter(client);	
+	}
+	else if(inputString[i] == 'I' || inputString[i] == 'i')
+	{
+		alphabet.drawI(client);
+		alphabet.nextLetter(client);	
+	}
+}
+
+// //Main commands
+// client.takeoff();
+// alphabet.init(client);
+// alphabet.drawM(client);
+// alphabet.nextLetter(client);
+// alphabet.drawL(client);
+// alphabet.nextLetter(client);
+// alphabet.drawH(client);
 
 //Stop and land
 client.after(500,function(){
